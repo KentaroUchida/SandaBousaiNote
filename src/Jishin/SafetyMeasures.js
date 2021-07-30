4b4d0f9b757acfce60572d2c578e2f106d944b9e
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-07-30 14:46:44 +0900

    fix: img show

--- src/Jishin/SafetyMeasures.js
+++ src/Jishin/SafetyMeasures.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Typography, Card, CardContent, CardMedia, Paper, makeStyles } from "@material-ui/core";
-import shijin_taishin1 from "./img/shijin_taishin1.png"
 
 const useStyles = makeStyles({
   cardTitle: {
@@ -34,7 +33,7 @@ export const SafetyMeasures = () => {
       <Typography variant="h3">
         たいせつな家族の命を守る「安全対策」の例
       </Typography>
-      <Measure title="家具の固定" text="L字金具などで固定する。2段重ねの家具は、つなぎ目を金具で連結させる。" image={shijin_taishin1}/>
+      <Measure title="家具の固定" text="L字金具などで固定する。2段重ねの家具は、つなぎ目を金具で連結させる。" image="/img/shijin_taishin1.png"/>
       <Measure title="家具の配置を工夫" text="(特に子どもや高齢者の部屋)倒れても下敷きにならない家具の配置にする。寝室にスリッパや靴を置いておく。"/>
       <Measure title="ガラスの飛散防止" text="窓ガラスに飛散防止フィルムをはる。または、強化ガラスに替える。食器棚のガラスにも飛散防止フィルムをはる。"/>
       <Measure title="収納" text="家具の上など、高いところに重いものを置かない。重いものは家具の下部に、軽いものは上部に収納する。"/>
