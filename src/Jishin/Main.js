70ad8a36ba09ba1f1e107646c8b0b811bd770865
youheisunada <yousuna@icloud.com>
2021-07-30 14:38:01 +0900

    add PrecautionList

--- src/Jishin/Main.js
+++ src/Jishin/Main.js
@@ -1,10 +1,15 @@
 import  React from 'react'
 import {Title} from './Title'
 import { ActionList } from './ActionList'
-
+import { PrecautionList1 } from './PrecautionList1'
+import { PrecautionList2 } from './PrecautionList2'
+import { PrecautionList3 } from './PrecautionList3'
 export const Jishin = () => {
     return(<>
         <Title/>
         <ActionList/>
+        <PrecautionList1/>
+        <PrecautionList2/>
+        <PrecautionList3/>
     </>)
 }
