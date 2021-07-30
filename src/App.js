ad6a6c5e48967a562150399721c6cdf28cd88b3e
Your Name <you@example.com>
2021-07-30 11:16:42 +0900

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//import Home from './Home'
+//import Form from './Form'
+//import Jishin from './Jishin'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return {
+  // (
+  //  <div className="App">
+  //    <BrowserRouter>
+  //      <Route exact path='/'>
+  //        <Home/>
+  //      </Route>
+  //      <Route exact path='/form'>
+  //        <Form/>
+  //      </Route>
+  //      <Route exact path='/jishin'>
+  //        <Jishin/>
+  //      </Route>
+  //    </BrowserRouter>
+  // </div>
+  //);
+  }
 }
 
 export default App;
