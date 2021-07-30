6ba6a27228d0abab3070a64cf55395af9005ca28
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-07-30 14:35:05 +0900

    add: safety measures

--- src/Jishin/Main.js
+++ src/Jishin/Main.js
@@ -1,10 +1,12 @@
 import  React from 'react'
 import {Title} from './Title'
 import { ActionList } from './ActionList'
+import {SafetyMeasures} from './SafetyMeasures'
 
 export const Jishin = () => {
     return(<>
         <Title/>
         <ActionList/>
+        <SafetyMeasures/>
     </>)
 }
