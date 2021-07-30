db7bb8bd4ac31aea60cdba18458e854eeb46d9a0
YamadaKouki <215x022x@stu.kobe-u.ac.jp>
2021-07-30 11:37:26 +0900

    form.jsを作成

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
 //import Home from './Home'
-//import Form from './Form'
+import Form from './Form'
 //import Jishin from './Jishin'
 
 function App() {
-  return {
-  // (
+  return (
   //  <div className="App">
   //    <BrowserRouter>
   //      <Route exact path='/'>
   //        <Home/>
   //      </Route>
   //      <Route exact path='/form'>
-  //        <Form/>
+          <Form/>
   //      </Route>
   //      <Route exact path='/jishin'>
   //        <Jishin/>
   //      </Route>
   //    </BrowserRouter>
   // </div>
-  //);
-  }
+  );
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,3 @@
+export default function Form() {
+    return (<p>Test</p>);
+}
