3dc7f4569ea9a2f6f284ca8765adc69573bfcf17
KentaroUchida <219x003x@stu.kobe-u.ac.jp>
2021-07-30 16:01:00 +0900

    commit menu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
-//import Home from './Home'
+import Home from './Home'
 //import Form from './Form'
+import Menu from './Menu'
+import Menu2 from './Menu2'
 //import Jishin from './Jishin'
 
 function App() {
-  return {
-  // (
-  //  <div className="App">
-  //    <BrowserRouter>
-  //      <Route exact path='/'>
-  //        <Home/>
-  //      </Route>
-  //      <Route exact path='/form'>
-  //        <Form/>
-  //      </Route>
-  //      <Route exact path='/jishin'>
-  //        <Jishin/>
-  //      </Route>
-  //    </BrowserRouter>
-  // </div>
-  //);
-  }
+  return (
+    //<div className="App">
+    //  <BrowserRouter>
+    //    <Route exact path='/'>
+    //      <Home/>
+    //      <Menu/>
+    //    </Route>
+    //    <Route exact path='/form'>
+    //      <Form/>
+    //        <Menu/>
+    //    </Route>
+    //    <Route exact path='/jishin'>
+    //      <Jishin/>
+    //        <Menu/>
+    //    </Route>
+    //  </BrowserRouter>
+    //</div>
+
+    //大事なとこ
+    //<div className="App">
+    //  <Home>
+    //    <Menu title="Home"/>
+    //  </Home>
+    //</div>
+
+    <div className="App">
+      <Menu title="Home">
+        <Home/>
+      </Menu>
+    </div>
+  );
 }
 
 export default App;
