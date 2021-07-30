67d4af759efb5557470f730b6614743a73572684
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-07-30 15:08:01 +0900

    add: measure imgs

--- src/Jishin/SafetyMeasures.js
+++ src/Jishin/SafetyMeasures.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { Typography, Card, CardContent, CardMedia, Paper, makeStyles } from "@material-ui/core";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Paper,
+  makeStyles,
+  Grid,
+} from "@material-ui/core";
 
 const useStyles = makeStyles({
+  card: {
+    height: 450,
+  },
   cardTitle: {
     backgroundColor: "#ffc0cb",
-  },
-  media: {
-    height: "100%"
   }
 });
 
 const Measure = ({ title, text, image }) => {
   const classes = useStyles();
   return (
-    <Card>
+    <Card className={classes.card}>
       <CardContent>
         <Paper elevation={3} className={classes.cardTitle}>
           <Typography variant="h5">{title}</Typography>
@@ -21,7 +29,7 @@ const Measure = ({ title, text, image }) => {
         {text}
       </CardContent>
       <CardMedia>
-        <img src={image} alt="" style={{height: 200}}/>
+        <img src={image} alt="" style={{ width: "100%" }} />
       </CardMedia>
     </Card>
   );
@@ -33,10 +41,36 @@ export const SafetyMeasures = () => {
       <Typography variant="h3">
         たいせつな家族の命を守る「安全対策」の例
       </Typography>
-      <Measure title="家具の固定" text="L字金具などで固定する。2段重ねの家具は、つなぎ目を金具で連結させる。" image="/img/shijin_taishin1.png"/>
-      <Measure title="家具の配置を工夫" text="(特に子どもや高齢者の部屋)倒れても下敷きにならない家具の配置にする。寝室にスリッパや靴を置いておく。"/>
-      <Measure title="ガラスの飛散防止" text="窓ガラスに飛散防止フィルムをはる。または、強化ガラスに替える。食器棚のガラスにも飛散防止フィルムをはる。"/>
-      <Measure title="収納" text="家具の上など、高いところに重いものを置かない。重いものは家具の下部に、軽いものは上部に収納する。"/>
+      <Grid container spacing={3}>
+          <Grid item xs={6}>
+      <Measure
+        title="家具の固定"
+        text="L字金具などで固定する。2段重ねの家具は、つなぎ目を金具で連結させる。"
+        image="/img/shijin_taishin1.png"
+      />
+      </Grid>
+          <Grid item xs={6}>
+      <Measure
+        title="家具の配置を工夫"
+        text="(特に子どもや高齢者の部屋)倒れても下敷きにならない家具の配置にする。寝室にスリッパや靴を置いておく。"
+        image="/img/kagu_haiti.png"
+      />
+      </Grid>
+          <Grid item xs={6}>
+      <Measure
+        title="ガラスの飛散防止"
+        text="窓ガラスに飛散防止フィルムをはる。または、強化ガラスに替える。食器棚のガラスにも飛散防止フィルムをはる。"
+        image="/img/curtain_pink.png"
+      />
+      </Grid>
+          <Grid item xs={6}>
+      <Measure
+        title="収納"
+        text="家具の上など、高いところに重いものを置かない。重いものは家具の下部に、軽いものは上部に収納する。"
+        image="/img/kagu_nimotsu_orosu.png"
+      />
+      </Grid>
+      </Grid>
     </>
   );
 };
