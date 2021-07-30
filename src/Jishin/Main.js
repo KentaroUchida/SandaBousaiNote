609161eadac526f1a54006917570e41cec1db0d0
youheisunada <yousuna@icloud.com>
2021-07-30 12:00:31 +0900

    add actionlist

--- src/Jishin/Main.js
+++ src/Jishin/Main.js
@@ -1,8 +1,10 @@
 import  React from 'react'
 import {Title} from './Title'
+import { ActionList } from './ActionList'
 
 export const Jishin = () => {
     return(<>
         <Title/>
+        <ActionList/>
     </>)
 }
