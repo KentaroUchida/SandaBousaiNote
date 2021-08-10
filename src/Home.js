af9f2896a843c8775cede8036ed20d9305efdfcd
KentaroUchida <219x003x@stu.kobe-u.ac.jp>
2021-08-10 09:10:02 +0900

    マインスイーパーコメントアウト

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Menu } from '@material-ui/core';
-import "react-minesweeper/lib/minesweeper.css";
+//import "react-minesweeper/lib/minesweeper.css";
 import Minesweeper from 'react-minesweeper'
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,13 +31,13 @@ export default function Home(props) {
     return (
         <div>
             {list}
-            <Minesweeper
+            {/* <Minesweeper
                 onWin={() => console.log("GAME WON")}
                 onLose={() => console.log("GAME LOST")}
                 bombChance={0.15} // 15% chance that a field will contain a bomb
                 width={10} // amount of fields horizontally
                 height={10} // amount of fields vertically
-            />
+            /> */}
         </div>
     );
     //return (
