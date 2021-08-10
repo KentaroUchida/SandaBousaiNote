bfcd233198bde17dc12165fc0325e67385446e6f
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-08-10 10:55:39 +0900

    fix: menubar close

--- src/Menu.js
+++ src/Menu.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import ListIcon from '@material-ui/icons/List';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import PrintIcon from '@material-ui/icons/Print';
-import WarningIcon from '@material-ui/icons/WarningSharp';
-import HomeIcon from '@material-ui/icons/HomeSharp';
-import DirectionsRunSharpIcon from '@material-ui/icons/DirectionsRunSharp';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import ListIcon from "@material-ui/icons/List";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import PrintIcon from "@material-ui/icons/Print";
+import WarningIcon from "@material-ui/icons/WarningSharp";
+import HomeIcon from "@material-ui/icons/HomeSharp";
+import DirectionsRunSharpIcon from "@material-ui/icons/DirectionsRunSharp";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
-import Grid from '@material-ui/core/Grid';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Grid from "@material-ui/core/Grid";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 const drawerWidth = 350;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   drawer: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: drawerWidth,
       flexShrink: 0,
     },
   },
   appBar: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`,
       marginLeft: drawerWidth,
     },
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
     },
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
@@ -84,70 +84,72 @@ export default function ResponsiveDrawer(props) {
   };
 
   const icons = [
-    <HomeIcon/>,<WarningIcon/>,<DirectionsRunSharpIcon/>,<WarningIcon/>
+    <HomeIcon />,
+    <WarningIcon />,
+    <DirectionsRunSharpIcon />,
+    <WarningIcon />,
   ];
 
-  const links = [
-    "/","/jishin","/jishin","/form"
-  ];
+  const links = ["/", "/jishin", "/jishin", "/form"];
 
   const drawer = (
     <div>
       <div className={classes.toolbar} />
       <List>
-        {['Home','グラっと地震が来たら！いのちを守る！できるだけケガをせず生き残る！','揺れがおさまったら','緊急時のわがやの情報'].map((text, index) => (
-            <Link to={links[index]} key={index}>
-              <Divider/>
-                <ListItem button >
-                    <ListItemIcon>
-                        {icons[index]}
-                    </ListItemIcon>
-                <ListItemText primary={text} />
-                </ListItem>
-            </Link>
+        {[
+          "Home",
+          "グラっと地震が来たら！いのちを守る！できるだけケガをせず生き残る！",
+          "揺れがおさまったら",
+          "緊急時のわがやの情報",
+        ].map((text, index) => (
+          <Link to={links[index]} key={index}>
+            <Divider />
+            <ListItem button onClick={handleDrawerClose}>
+              <ListItemIcon>{icons[index]}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          </Link>
         ))}
-        <Divider/>
+        <Divider />
       </List>
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar 
+      <AppBar
         position="fixed"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: mobileOpen,
-        })}>
+        })}
+      >
         <Toolbar>
-          <Grid
-            justify="space-between"
-            alignItems="center"
-            container
-            >
-              <div style={{display:"inline-flex",}}>
-                  <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    edge="start"
-                    onClick={handleDrawerOpen}
-                    className={clsx(classes.menuButton, mobileOpen && classes.hide)}
-                  >
-                    <ListIcon />
-                  </IconButton>
-                  <Typography variant="h6" noWrap style={{marginTop:"8px"}}>
-                    {props.title}
-                  </Typography>
-              </div>
+          <Grid justify="space-between" alignItems="center" container>
+            <div style={{ display: "inline-flex" }}>
               <IconButton
                 color="inherit"
-                edge="end"
-                // onClick={handleDrawerOpen}
-                >
-                  <PrintIcon />
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerOpen}
+                className={clsx(classes.menuButton, mobileOpen && classes.hide)}
+              >
+                <ListIcon />
               </IconButton>
+              <Typography variant="h6" noWrap style={{ marginTop: "8px" }}>
+                {props.title}
+              </Typography>
+            </div>
+            <IconButton
+              color="inherit"
+              edge="end"
+              // onClick={handleDrawerOpen}
+            >
+              <PrintIcon />
+            </IconButton>
           </Grid>
         </Toolbar>
       </AppBar>
@@ -157,7 +159,7 @@ export default function ResponsiveDrawer(props) {
           <Drawer
             container={container}
             variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            anchor={theme.direction === "rtl" ? "right" : "left"}
             open={mobileOpen}
             onClose={handleDrawerToggle}
             classes={{
@@ -168,9 +170,13 @@ export default function ResponsiveDrawer(props) {
             }}
           >
             <div className={classes.drawerHeader}>
-                <IconButton onClick={handleDrawerClose}>
-                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                </IconButton>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === "ltr" ? (
+                  <ChevronLeftIcon />
+                ) : (
+                  <ChevronRightIcon />
+                )}
+              </IconButton>
             </div>
             {drawer}
           </Drawer>
@@ -189,9 +195,7 @@ export default function ResponsiveDrawer(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-            {props.children}
-        </Typography>
+        <Typography paragraph>{props.children}</Typography>
       </main>
     </div>
   );
