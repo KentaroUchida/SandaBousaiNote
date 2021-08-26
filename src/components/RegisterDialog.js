f90a9f36893b273bedca81af70208fb56cd9764a
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-08-26 10:18:57 +0900

    update: form import sentences

--- src/components/RegisterDialog.js
+++ src/components/RegisterDialog.js
@@ -1,13 +1,6 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import IconButton from "@material-ui/core/IconButton";
-import AddCircleIcon from "@material-ui/icons/AddCircle";
+import {Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton} from "@material-ui/core";
+import {AddCircle as AddCircleIcon} from "@material-ui/icons";
 
 const categoryTextList = {
   family: "家族",
--- src/pages/P0Form.js
+++ src/pages/P0Form.js
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
+import {Accordion, AccordionSummary, AccordionDetails, Typography, TextField, Grid, Button} from "@material-ui/core";
+import {ExpandMore as ExpandMoreIcon} from "@material-ui/icons";
 import FormRegisterDialog from "../components/RegisterDialog";
 import FormEditDialog from "../components/EditDialog";
-import Button from "@material-ui/core/Button";
 
 const Form = () => {
   const [familyList, setFamilyList] = useState(
