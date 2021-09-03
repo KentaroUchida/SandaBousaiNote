56b0698352ded85cab25aa51875e6fddeb39d1c8
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-03 18:19:53 +0900

    update: 段階移行を調整

--- src/pages/P7Izanigeru.js
+++ src/pages/P7Izanigeru.js
@@ -10,27 +10,27 @@ import Typography from '@material-ui/core/Typography';
 import { ResponsiveFontProvider } from "../components/ResponsiveFontProvider"
 import Button from '@material-ui/core/Button';
 import {
-    createTheme,
-    responsiveFontSizes,
-    ThemeProvider,
-  } from '@material-ui/core/styles';
+	createTheme,
+	responsiveFontSizes,
+	ThemeProvider,
+} from '@material-ui/core/styles';
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
 const Title = () => {
-    return(
-        <ThemeProvider theme={theme}>
-            <ResponsiveFontProvider>
-                <Typography variant="h2" gutterBottom>
-                    いざ逃げる！
-                </Typography>
-                <Typography variant="h4" gutterBottom>
-                    災害によって、避難場所がちがうよ!
-                </Typography>
-            </ResponsiveFontProvider>
-        </ThemeProvider>
-        );
+	return (
+		<ThemeProvider theme={theme}>
+			<ResponsiveFontProvider>
+				<Typography variant="h2" gutterBottom>
+					いざ逃げる！
+				</Typography>
+				<Typography variant="h4" gutterBottom>
+					災害によって、避難場所がちがうよ!
+				</Typography>
+			</ResponsiveFontProvider>
+		</ThemeProvider>
+	);
 }
 
 const measureStyles = makeStyles((theme) => ({
@@ -56,7 +56,7 @@ const Measure = ({ title, image, text }) => {
 				<img src={image} alt="" style={{ maxWidth: "100%", height: "auto" }} />
 			</CardMedia>
 			<CardContent>
-					{text}
+				{text}
 			</CardContent>
 		</Card>
 	);
@@ -94,7 +94,7 @@ const Shelter = () => {
 };
 
 const Sitteta = () => {
-	return(<>
+	return (<>
 		<Grid>
 			<Grid>
 				<img src="/img/pages/P7Izanigeru/sitteta.png" alt="" style={{ maxWidth: "30%", height: "auto" }} />
@@ -113,21 +113,21 @@ const Sitteta = () => {
 	</>)
 }
 
-const CheckPlace = () =>{
-	return(<>
-		<img alt="" src="/img/pages/P7Izanigeru/pict.png" style={{ maxWidth: "100%", height: "auto" }}/>
-		</>
+const CheckPlace = () => {
+	return (<>
+		<img alt="" src="/img/pages/P7Izanigeru/pict.png" style={{ maxWidth: "100%", height: "auto" }} />
+	</>
 	);
 };
 
-const Notokiha = () =>{
+const Notokiha = () => {
 	const classes = measureStyles();
-	return(<>
-		<Grid  container spacing={3} direction="row">
+	return (<>
+		<Grid container spacing={3} direction="row">
 			<Grid item xs={6} className={classes.grid}>
 				<Card>
 					<CardMedia>
-						<img alt="" src="/img/pages/P7Izanigeru/kouzui.png" style={{ maxWidth: "100%", height: "auto" }}/>
+						<img alt="" src="/img/pages/P7Izanigeru/kouzui.png" style={{ maxWidth: "100%", height: "auto" }} />
 					</CardMedia>
 					<CardContent>
 						洪水・津波の時は…<br></br>
@@ -139,7 +139,7 @@ const Notokiha = () =>{
 			<Grid item xs={6} className={classes.grid}>
 				<Card>
 					<CardMedia>
-						<img alt="" src="/img/pages/P7Izanigeru/dosya.png" style={{ maxWidth: "100%", height: "auto" }}/>
+						<img alt="" src="/img/pages/P7Izanigeru/dosya.png" style={{ maxWidth: "100%", height: "auto" }} />
 					</CardMedia>
 					<CardContent>
 						土砂の時は…<br></br>
@@ -147,103 +147,103 @@ const Notokiha = () =>{
 					</CardContent>
 				</Card>
 			</Grid>
-		
+
 		</Grid>
 	</>)
 }
 
-const HinanCard = () =>{
-	return(<>
-	<Card>
+const HinanCard = () => {
+	return (<>
+		<Card>
 			<ResponsiveFontProvider>
 				<Typography variant="h3" gutterBottom>
 					わがやの災害避難カードを作ろう！
 				</Typography>
 			</ResponsiveFontProvider>
 			<p>水害、土砂、地震、火災など災害によって複数の逃げる場所を持つことが大切です。</p>
-			<img alt="" src="/img/pages/P7Izanigeru/bousaiMap.png" style={{ maxWidth: "80%", height: "auto"}}/>
+			<img alt="" src="/img/pages/P7Izanigeru/bousaiMap.png" style={{ maxWidth: "80%", height: "auto" }} />
 		</Card>
 	</>)
 }
 
 class ShelterInformation extends React.Component {
-	constructor(){
+	constructor() {
 		super();
-		this.state={
+		this.state = {
 
 		}
 		this.setValues = this.setValues.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
-	handleChange(event){
-		this.setState({[event.target.id]:event.target.value});
+	handleChange(event) {
+		this.setState({ [event.target.id]: event.target.value });
 	}
-	setValues(){
-		if(this.state.suigai1!==undefined) localStorage.setItem('suigai1',this.state.suigai1);
-		if(this.state.suigai2!==undefined) localStorage.setItem('suigai2',this.state.suigai2);
-		if(this.state.dosya1!==undefined)  localStorage.setItem('dosya1',this.state.dosya1);
-		if(this.state.dosya2!==undefined) localStorage.setItem('dosya2',this.state.dosya2);
-		if(this.state.jishin1!==undefined) localStorage.setItem('jishin1',this.state.jishin1);
-		if(this.state.jishin2!==undefined) localStorage.setItem('jishin2',this.state.jishin2);
-		if(this.state.kasai1!==undefined) localStorage.setItem('kasai1',this.state.kasai1);
-		if(this.state.kasai2!==undefined) localStorage.setItem('kasai2',this.state.kasai2);
+	setValues() {
+		if (this.state.suigai1 !== undefined) localStorage.setItem('suigai1', this.state.suigai1);
+		if (this.state.suigai2 !== undefined) localStorage.setItem('suigai2', this.state.suigai2);
+		if (this.state.dosya1 !== undefined) localStorage.setItem('dosya1', this.state.dosya1);
+		if (this.state.dosya2 !== undefined) localStorage.setItem('dosya2', this.state.dosya2);
+		if (this.state.jishin1 !== undefined) localStorage.setItem('jishin1', this.state.jishin1);
+		if (this.state.jishin2 !== undefined) localStorage.setItem('jishin2', this.state.jishin2);
+		if (this.state.kasai1 !== undefined) localStorage.setItem('kasai1', this.state.kasai1);
+		if (this.state.kasai2 !== undefined) localStorage.setItem('kasai2', this.state.kasai2);
 		alert("避難先情報を保存しました")
 	}
-	render(){
-		let suigai1=localStorage.getItem('suigai1');
-		let suigai2=localStorage.getItem('suigai2');
-		let dosya1=localStorage.getItem('dosya1');
-		let dosya2=localStorage.getItem('dosya2');
-		let jishin1=localStorage.getItem('jishin1');
-		let jishin2=localStorage.getItem('jishin2');
-		let kasai1=localStorage.getItem('kasai1');
-		let kasai2=localStorage.getItem('kasai2');
+	render() {
+		let suigai1 = localStorage.getItem('suigai1');
+		let suigai2 = localStorage.getItem('suigai2');
+		let dosya1 = localStorage.getItem('dosya1');
+		let dosya2 = localStorage.getItem('dosya2');
+		let jishin1 = localStorage.getItem('jishin1');
+		let jishin2 = localStorage.getItem('jishin2');
+		let kasai1 = localStorage.getItem('kasai1');
+		let kasai2 = localStorage.getItem('kasai2');
 
-		return(
-		<div>
-		<table border="1">
-			<tr>
-				<th></th>
-				<th>まず逃げるところ</th>
-				<th>家に帰れないときに過ごす場所</th>
-			</tr>
-			<tr>
-				<th>水害</th>
-				<th><TextField onChange={this.handleChange} id="suigai1" defaultValue={suigai1}/></th>
-				<th><TextField onChange={this.handleChange} id="suigai2" defaultValue={suigai2}/></th>
-			</tr>
-			<tr>
-				<th>土砂</th>
-				<th><TextField onChange={this.handleChange} id="dosya1" defaultValue={dosya1}/></th>
-				<th><TextField onChange={this.handleChange} id="dosya2" defaultValue={dosya2}/></th>
-			</tr>
-			<tr>
-				<th>地震</th>
-				<th><TextField onChange={this.handleChange} id="jishin1" defaultValue={jishin1}/></th>
-				<th><TextField onChange={this.handleChange} id="jishin2" defaultValue={jishin2}/></th>
-			</tr>
-			<tr>
-				<th>火災</th>
-				<th><TextField onChange={this.handleChange} id="kasai1" defaultValue={kasai1}/></th>
-				<th><TextField onChange={this.handleChange} id="kasai2" defaultValue={kasai2}/></th>
-			</tr>
-		</table>
-		<Button onClick={this.setValues} variant="contained" color="primary">
-      保存
-    </Button>
-		</div>
+		return (
+			<div>
+				<table border="1">
+					<tr>
+						<th></th>
+						<th>まず逃げるところ</th>
+						<th>家に帰れないときに過ごす場所</th>
+					</tr>
+					<tr>
+						<th>水害</th>
+						<th><TextField onChange={this.handleChange} id="suigai1" defaultValue={suigai1} /></th>
+						<th><TextField onChange={this.handleChange} id="suigai2" defaultValue={suigai2} /></th>
+					</tr>
+					<tr>
+						<th>土砂</th>
+						<th><TextField onChange={this.handleChange} id="dosya1" defaultValue={dosya1} /></th>
+						<th><TextField onChange={this.handleChange} id="dosya2" defaultValue={dosya2} /></th>
+					</tr>
+					<tr>
+						<th>地震</th>
+						<th><TextField onChange={this.handleChange} id="jishin1" defaultValue={jishin1} /></th>
+						<th><TextField onChange={this.handleChange} id="jishin2" defaultValue={jishin2} /></th>
+					</tr>
+					<tr>
+						<th>火災</th>
+						<th><TextField onChange={this.handleChange} id="kasai1" defaultValue={kasai1} /></th>
+						<th><TextField onChange={this.handleChange} id="kasai2" defaultValue={kasai2} /></th>
+					</tr>
+				</table>
+				<Button onClick={this.setValues} variant="contained" color="primary">
+					保存
+				</Button>
+			</div>
 		)
 	}
 }
 
-export const P7Izanigeru= () => {
-	return(<>
-		<Title/>
-		<Shelter/>
-		<Sitteta/>
-		<CheckPlace/>
-		<Notokiha/>
-		<HinanCard/>
-		<ShelterInformation/>
+export const P7Izanigeru = () => {
+	return (<>
+		<Title />
+		<Shelter />
+		<Sitteta />
+		<CheckPlace />
+		<Notokiha />
+		<HinanCard />
+		<ShelterInformation />
 	</>);
 }
