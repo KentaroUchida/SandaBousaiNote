76dc03447917a42c263ab6e7408df6ae17596e06
Takuya Nakata Home <tnakata@ws.cs.kobe-u.ac.jp>
2021-10-13 10:14:56 +0900

    update: p3 jishin fin

--- src/pages/P3Jishin.js
+++ src/pages/P3Jishin.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import { CardContent } from "@mui/material";
 import {
-  ImageCard,
   ImageTextCard,
   TitleTextImageCard,
+  TitleCardPart,
+  CardBase,
+  ImageCardPart,
 } from "../components/CardComponents";
 import { Title, SubTitle, BodyText } from "../components/TitleComponents";
 
-const img_path = "/img/pages/P3Jishin"
+const img_path = "/img/pages/P3Jishin";
+const generateImagePath = (filename) => img_path + "/" + filename;
 
 export const P3Jishin = () => {
   return (
@@ -18,37 +21,34 @@ export const P3Jishin = () => {
         <br />
         できるだけケガをせず生き残る！
       </SubTitle>
-      <ImageCard image={`${img_path}/family_under_table.png`} />
-      <BodyText>
-        1.姿勢を低く
+      <ImageTextCard image={generateImagePath("family_under_table.png")}>
+        1. 姿勢を低く
         <br />
-        2.頭を守る(机の下など)
+        2. 頭を守る(机の下など)
         <br />
-        3.ゆれがおさまったら避難する
-      </BodyText>
+        3. ゆれがおさまったら避難する
+      </ImageTextCard>
 
-      <ImageTextCard image={`${img_path}/jishin_tsukue.png`}>
+      <ImageTextCard image={generateImagePath("jishin_tsukue.png")}>
         テーブルや机などの下にもぐり身を守る。
         <br />
         足は出さないように注意！
       </ImageTextCard>
-      <ImageTextCard image={`${img_path}/gomi_waremono.png`}>
+      <ImageTextCard image={generateImagePath("gomi_waremono.png")}>
         キッチンにいるママは、子供の名前を呼んではいけません。
         <br />
         キッチンはガラスが割れて危険がいっぱい！
       </ImageTextCard>
-      <ImageTextCard image={`${img_path}/jiko_jishin_himoto.png`}>
+      <ImageTextCard image={generateImagePath("jiko_jishin_himoto.png")}>
         火の元よりもまずは自分の身を守ること。
         <br />
         震度５の揺れを感知すると自動的にガスはとまるようになっています。
       </ImageTextCard>
 
-      <SubTitle>
-        たいせつな家族の命を守る「安全対策」の例
-      </SubTitle>
+      <SubTitle>たいせつな家族の命を守る「安全対策」の例</SubTitle>
       <TitleTextImageCard
         title="家具の固定"
-        image={`${img_path}/shijin_taishin1.png`}
+        image={generateImagePath("shijin_taishin1.png")}
       >
         L字金具などで固定する。
         <br />
@@ -56,26 +56,32 @@ export const P3Jishin = () => {
       </TitleTextImageCard>
       <TitleTextImageCard
         title="家具の配置を工夫"
-        image={`${img_path}/kagu_haiti.png`}
+        image={generateImagePath("kagu_haiti.png")}
       >
         (特に子どもや高齢者の部屋)
-        <br/>
+        <br />
         倒れても下敷きにならない家具の配置にする。
         <br />
         寝室にスリッパや靴を置いておく。
       </TitleTextImageCard>
-      <TitleTextImageCard
-        title="ガラスの飛散防止"
-        image={`${img_path}/kagu_window.png`}
-      >
-        窓ガラスに飛散防止フィルムをはる。
-        または、強化ガラスに替える。
-        <br />
-        食器棚のガラスにも飛散防止フィルムをはる。
-      </TitleTextImageCard>
+      <CardBase>
+        <CardContent>
+          <TitleCardPart title="ガラスの飛散防止" />
+          <BodyText>
+            窓ガラスに飛散防止フィルムをはる。 または、強化ガラスに替える。
+            <br />
+            食器棚のガラスにも飛散防止フィルムをはる。
+          </BodyText>
+          <ImageCardPart image={generateImagePath("kagu_window.png")} />
+          <BodyText>
+            カーテンなどを閉めておくだけでもガラスの飛散を軽減できます。
+          </BodyText>
+          <ImageCardPart image={generateImagePath("curtain_pink.png")} />
+        </CardContent>
+      </CardBase>
       <TitleTextImageCard
         title="収納"
-        image={`${img_path}/kagu_nimotsu_orosu.png`}
+        image={generateImagePath("kagu_nimotsu_orosu.png")}
       >
         家具の上など、高いところに重いものを置かない。
         <br />
