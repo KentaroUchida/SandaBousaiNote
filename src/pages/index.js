e3eba2d241c78065390be9f968b55adb5086d6f8
tnakata <tnakata@ws.cs.kobe-u.ac.jp>
2021-10-28 17:53:03 +0900

    add: p14

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,7 @@ export * from "./SandaP10SandaSaigai"
 export * from "./SandaP11Daijobu";
 export * from "./SandaP12Goods1";
 export * from "./SandaP13Goods2";
+export * from "./SandaP14Foods1";
 export * from "./SandaP17Toilet";
 export * from "./SandaP18Bousaikaigi";
 export * from "./SandaP19BousaiSanpo";
