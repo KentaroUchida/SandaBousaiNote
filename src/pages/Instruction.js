8c358207d0248c3691d5ed87693eeecd899c5be0
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-10-29 16:09:42 +0900

    update: 画像を修正

--- src/pages/Instruction.js
+++ src/pages/Instruction.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {
+  CardContent,
   Grid,
   ListItem,
   ListItemIcon,
   ListItemText,
   Typography,
 } from "@mui/material";
+import {
+  CardBase,
+  TitleCardPart,
+} from "../components/CardComponents";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 // import {
 // 	createTheme,
@@ -24,36 +29,28 @@ const instructionText = [
   "印刷ボタンを押すとPDFを作成することができます",
 ];
 
-class Title extends React.Component {
-  render() {
-    return (
-      <div>
-        <Grid container alignItems="center" justify="center">
-          <h2>さんだ防災ノートの使い方</h2>
-        </Grid>
-      </div>
-    );
-  }
-}
 class Content extends React.Component {
   render() {
     return (
-      <div>
-        {instructionText.map((text, i) => {
-          return (
-            <div>
-              <Typography>
-                <ListItem>
-                  <ListItemIcon>
-                    <FiberManualRecordIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={instructionText[i]} />
-                </ListItem>
-              </Typography>
-            </div>
-          );
-        })}
-      </div>
+      <CardBase>
+        <CardContent>
+          <TitleCardPart title="さんだ防災ノートの使い方" />
+          {instructionText.map((text, i) => {
+            return (
+              <div>
+                <Typography>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FiberManualRecordIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={instructionText[i]} />
+                  </ListItem>
+                </Typography>
+              </div>
+            );
+          })}
+        </CardContent>
+      </CardBase>
     );
   }
 }
@@ -61,7 +58,6 @@ class Content extends React.Component {
 function Instruction() {
   return (
     <>
-      <Title />
       <Content />
     </>
   );
